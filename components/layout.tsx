de9418b14bf728b12ae78d343b3c6ceca40a582a
mbetim <ma.betim@hotmail.com>
2020-10-17 01:11:06 -0300

    Convert the components to typescript

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,10 +3,14 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
+interface Props {
+  home?: boolean;
+}
+
 const name = "Your Name";
 export const siteTitle = "Next.js Sample Website";
 
-export default function Layout({ children, home }) {
+const Layout: React.FC<Props> = ({ home, children }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -62,4 +66,6 @@ export default function Layout({ children, home }) {
       )}
     </div>
   );
-}
+};
+
+export default Layout;
